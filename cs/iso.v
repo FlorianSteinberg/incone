01d256c43054dc1a4504bea2ac6d9003e914ec73
FlorianSteinberg <florian.steinberg@inria.fr>
2018-11-06 01:47:45 +0100

    added proofs that isomorphism is equivalence relation.

--- cs/iso.v
+++ cs/iso.v
@@ -1,13 +1,13 @@
 From mathcomp Require Import all_ssreflect.
 Require Import all_core cs func facts.
 Require Import FunctionalExtensionality ClassicalFacts ClassicalChoice Psatz ProofIrrelevance.
-Require Import Morphisms.
+Require Import RelationClasses.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Section ISOMORPHISMS.
+Section isomorphisms.
 Definition isomorphism (X Y: cs) (f: X c-> Y) :=
 	exists (g: Y c-> X), cancel (projT1 f) (projT1 g) /\ cancel (projT1 g) (projT1 f).
 
@@ -16,19 +16,21 @@ Arguments isomorphic {X Y}.
 
 Notation "X ~=~ Y" := (@isomorphic X Y) (at level 2).
 
-Lemma iso_ref X: X ~=~ X.
-Proof. by exists (exist_c (id_cont X)); exists (exist_c (id_cont X)). Qed.
+Lemma iso_ref: Reflexive (@isomorphic).
+Proof. by move => X; exists (exist_c (id_cont X)); exists (exist_c (id_cont X)). Qed.
 
-Lemma iso_sym X Y: X ~=~ Y -> Y ~=~ X.
-Proof. by move => [f [g [cncl cncl']]]; exists g; exists f. Qed.
+Lemma iso_sym: Symmetric (@isomorphic).
+Proof. by move => X Y [f [g [cncl cncl']]]; exists g; exists f. Qed.
 
-Lemma iso_trans X Y Z (someqx: questions X) (someqz: questions Z):
-	X ~=~ Y -> Y ~=~ Z -> X ~=~ Z.
+Lemma iso_trans: Transitive (@isomorphic).
 Proof.
-move => [f [g [cncl1 cncl2]]] [f' [g' [cncl1' cncl2']]].
+move => X Y Z [f [g [cncl1 cncl2]]] [f' [g' [cncl1' cncl2']]].
 exists (exist_c (cont_comp (projT2 f') (projT2 f))).
 exists (exist_c (cont_comp (projT2 g) (projT2 g'))).
 by rewrite /=; split; apply can_comp.
 Qed.
-End ISOMORPHISMS.
+
+Global Instance iso_equiv: Equivalence (@isomorphic).
+Proof. split; [exact/iso_ref | exact/iso_sym | exact/iso_trans]. Qed.
+End isomorphisms.
 Notation "X ~=~ Y" := (@isomorphic X Y) (at level 2).

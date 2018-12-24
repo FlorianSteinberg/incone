d735cef40c6a4f598715c24d8636313957748730
Florian Steinberg <florian.steinberg@inria.fr>
2018-12-24 13:52:38 +0100

    some compatibility with classes from coquelicot.

---
+++ cs/mclct.v
@@ -0,0 +1,11 @@
+(* Interface of MetricSpace type with types from the Coqeulicot Hierarchy *)
+From mathcomp Require Import all_ssreflect.
+From rlzrs Require Import all_rlzrs.
+Require Import all_core.
+From Coquelicot Require Import Hierarchy.
+Import Morphisms.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+

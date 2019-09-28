ec986e738d33eb1680483e8e31ca12e685f70627
Florian Steinberg <florian.steinberg@inria.fr>
2019-09-28 15:17:29 +0200

    added proof-irrelevance and the important conclusion from it.

--- axioms.v
+++ axioms.v
@@ -1,5 +1,12 @@
-From mathcomp Require Import ssrfun.
-Require Import Classical ChoiceFacts count.
+From mathcomp Require Import ssreflect ssrfun.
+Require Import Classical ChoiceFacts count ProofIrrelevance.
+
+Fact eq_sub T P (a b : {x : T | P x}) : a = b <-> projT1 a = projT1 b.
+Proof.
+  split=> [->//|]; move: a b => [a Pa] [b Pb] /= eqab.
+  case: _ / eqab in Pb *; congr (exist _ _ _).
+  exact/proof_irrelevance.
+Qed.
 
 Axiom functional_extensionality: forall Q A (f g: Q -> A), f =1 g -> f = g.
 Notation fun_ext:= functional_extensionality.

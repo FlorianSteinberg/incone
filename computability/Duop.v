a6aa0b68133804e0f2c3771b57f1f26c4dc283e0
Florian Steinberg <florian.steinberg@inria.fr>
2019-09-30 16:36:01 +0200

    removed old files

--- computability/Duop.v
+++
@@ -1,8 +0,0 @@
-From mathcomp Require Import ssreflect seq ssrnat ssrbool eqtype ssrfun.
-From mf Require Import all_mf.
-Require Import all_cont FMop Umach Ucont.
-
-Set Implicit Arguments.
-Unset Strict Implicit.
-Unset Printing Implicit Defensive.
-
